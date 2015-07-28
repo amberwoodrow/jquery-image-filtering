b3ed6a2d4b26c9102647819ad28206affe0cac32
amberwoodrow <amberwoodrow@gmail.com>
2015-07-28 16:25:56 -0600

    appended new cabins, made back ground buttons functional, and added a button.

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-	console.log("The document is ready!");
-})
+  $(".image-container").append("<article class='home' data-price='400,000'><img src='img/eleven.jpg'/><h4>$400,000</h4></article>");
+  $(".image-container").append("<article class='home' data-price='40,000,000'><img src='img/eleven.jpg'/><h4>$40,000,000</h4></article>");
+  $(".image-container").append("<article class='home' data-price='35,000'><img src='img/eleven.jpg'/><h4>$35,000</h4></article>");
+
+  $(".dark").on("click", function() {
+    $("body").css("background", "url(img/dark_wood.png)");
+  });
+
+  $(".light").on("click", function() {
+    $("body").css("background", "url(img/tileable_wood_texture.png)");
+  });
+
+  $(".textures").append("<span class='forest'></span>");
+
+  $(".forest").on("click", function() {
+    $("body").css("background", "url(img/forest.jpg)");
+  });
+});
