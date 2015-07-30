222ea7bd14f01f4682bcc89cff4c00fcdb56a011
amberwoodrow <amberwoodrow@gmail.com>
2015-07-30 10:22:25 -0600

    added 3 functions, one to make array of prices, one to sort array from low to high, one to sort array from high to low.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  $(".image-container").append("<article class='home' data-price='400,000'><img src='img/eleven.jpg'/><h4>$400,000</h4></article>");
-  $(".image-container").append("<article class='home' data-price='40,000,000'><img src='img/eleven.jpg'/><h4>$40,000,000</h4></article>");
-  $(".image-container").append("<article class='home' data-price='35,000'><img src='img/eleven.jpg'/><h4>$35,000</h4></article>");
+  $(".image-container").append("<article class='home' data-price='400000'><img src='img/eleven.jpg'/><h4>$400,000</h4></article>");
+  $(".image-container").append("<article class='home' data-price='40000000'><img src='img/eleven.jpg'/><h4>$40,000,000</h4></article>");
+  $(".image-container").append("<article class='home' data-price='35000'><img src='img/eleven.jpg'/><h4>$35,000</h4></article>");
 
   $(".dark").on("click", function() {
     $("body").css("background", "url(img/dark_wood.png)");
@@ -16,4 +16,27 @@ $(document).ready(function() {
   $(".forest").on("click", function() {
     $("body").css("background", "url(img/forest.jpg)");
   });
+
+  function homeArr () {
+    var home = $(".home");
+    var homesArr = [];
+    for (i=0; i < home.length; i++) {
+     homesArr.push($(home[i]).data("price"));
+    }
+    return homesArr;
+  }
+
+  function lowToHigh () {
+    var homesArr = homeArr();
+    return homesArr.sort(function(a, b) {
+       return a - b;
+    });
+  }
+
+  function highToLow () {
+    var homesArr = homeArr();
+    return homesArr.sort(function(a, b) {
+       return b - a;
+    });
+  }
 });
